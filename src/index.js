bf2fd81ef3d8fa34f09ba43cb372da281a459911
Oliver Weber <oliver@mondough.io>
2019-02-19 21:08:28 +0100

    Starter Code

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./App"
+import registerServiceWorker from "./registerServiceWorker"
+
+import { createStore } from "redux"
+import { Provider } from "react-redux"
+
+import products from "./products.json"
+
+const appReducer = state => state
+/* ↑
+ * ↑ you should delete this line
+ * and create a proper reducer and import it.
+ * like this ↓
+ * import { appReducer } from "./modules/reducers"
+ */
+
+const store = createStore(
+  appReducer,
+  {
+    products,
+    cart: {}
+  },
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+)
+registerServiceWorker()
